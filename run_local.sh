bb27565994f6986b1ae82d27ec33a40ca314372c
Nico Ohnezat <nico@no-12.net>
2020-12-17 13:53:47 +0100

    use ansible-role-dotfiles for common dotfiles

--- run_local.sh
+++ run_local.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+ansible-galaxy role install --role-file requirements.yml --roles-path roles_external --force-with-deps
 ansible-playbook -v -i "$HOSTNAME", -e ansible_connection=local --ask-become-pass playbook.yml
