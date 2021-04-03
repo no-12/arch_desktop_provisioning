aa25f2d19f40376c404ce4965862e81c7dfc00b0
Nico Ohnezat <nico@no-12.net>
2021-04-03 21:50:18 +0200

    use direnv to setup python virtual environment

--- run_local.sh
+++ run_local.sh
@@ -1,25 +1,9 @@
 #!/usr/bin/env bash
-set -eu -o pipefail
+set -eu
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
-if [[ -d .python_venv ]]; then
-    source ".python_venv/bin/activate"
-else
-    python3 -m venv .python_venv
-    source ".python_venv/bin/activate"
-    pip install --upgrade pip
-    pip install --upgrade ansible
-fi
-
-(
-    cd $SCRIPT_DIR
-    ansible-galaxy role install --role-file requirements.yml --roles-path roles_external --force-with-deps
-    ansible-playbook \
-        --inventory "$HOSTNAME", \
-        --extra-vars "ansible_connection=local arch_user=$(whoami)" \
-        --ask-become-pass \
-        playbook.yml -v
-)
-
-deactivate
+ansible-galaxy role install --role-file requirements.yml --roles-path roles_external --force-with-deps
+ansible-playbook \
+    --inventory "$HOSTNAME", \
+    --extra-vars "ansible_connection=local arch_user=$(whoami)" \
+    --ask-become-pass \
+    playbook.yml -v
